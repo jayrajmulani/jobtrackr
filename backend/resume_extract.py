36bf70bd3705a87a75ac15d8153947086be3d71b
tndo <tndo@ncsu.edu>
2024-10-28 09:09:24 -0400

    added resumes to unit test

--- backend/resume_extract.py
+++ backend/resume_extract.py
@@ -2,17 +2,11 @@ import PyPDF2
 import os;
 
 def extract_text_from_pdf(pdf_path):
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
+    with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        text = ""
 
-    for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
-
-    pdf_file.close()
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
     return text
-
-pdf_path = 'Thien Do resume 11-25.pdf'
-extracted_text = extract_text_from_pdf(os.path.join("resume_examples", pdf_path))
-print(extracted_text)
